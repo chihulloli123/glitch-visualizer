8ebedef05c61d0ea367b10f02ddf162736fd8487
amandaPerez <chihulloli123@gmail.com>
2025-12-17 01:20:43 +0000

    core: replace hello world with render loop heartbeat

--- Visualizer/Program.cs
+++ Visualizer/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+Console.Clear();
+Console.CursorVisible = false;
+
+int frame = 0;
+
+while (true)
+{
+    Console.SetCursorPosition(0, 0);
+    Console.WriteLine($"glitch-visualizer running | frame {frame++}");
+    Thread.Sleep(1000 / 60); // 60 FPS heartbeat
+}
